e913d1e55c6a7507195dd755f6ac94546993493c
Siarhei Mashkevski <cybera@list.ru>
2018-06-19 13:08:25 +0300

    добавляет решение задачи #16 раздела #4

--- js/map.js
+++ js/map.js
@@ -268,9 +268,79 @@ mapPinMain.addEventListener('mouseup', function () {
   }
 });
 
-addForm.addEventListener('reset', function () {
+var addFormReset = document.querySelector('.ad-form__reset');
+var mapFilters = document.querySelector('.map__filters');
+addFormReset.addEventListener('click', function () {
+  mapFilters.reset();
+  addForm.reset();
   setInactiveState();
+  addressInput.value = getPinAddress(mapPinMain, PIN_WIDTH, PIN_MAIN_DEFAULT_HEIGHT);
 });
 
-setInactiveState();
+var changeMinPrice = function (typeValue) {
+  var priceInput = document.querySelector('#price');
+  switch (typeValue) {
+    case 'palace': priceInput.min = 10000; priceInput.placeholder = '10000';
+      break;
+    case 'flat': priceInput.min = 1000; priceInput.placeholder = '1000';
+      break;
+    case 'house': priceInput.min = 5000; priceInput.placeholder = '5000';
+      break;
+    case 'bungalo': priceInput.min = 0; priceInput.placeholder = '0';
+      break;
+    default: break;
+  }
+};
+
+var type = document.querySelector('#type');
+
+type.addEventListener('change', function () {
+  changeMinPrice(type.value);
+});
+
+var timeInSelect = document.querySelector('#timein');
+var timeOutSelect = document.querySelector('#timeout');
+
+timeInSelect.addEventListener('change', function () {
+  var selectedIindex = timeInSelect.selectedIndex;
+  var options = timeOutSelect.options;
+  options[selectedIindex].selected = true;
+});
+
+timeOutSelect.addEventListener('change', function () {
+  var selectedIindex = timeOutSelect.selectedIndex;
+  var options = timeInSelect.options;
+  options[selectedIindex].selected = true;
+});
+
+var roomNumberSelect = document.querySelector('#room_number');
+var capacitySelect = document.querySelector('#capacity');
+
+var validateRoomsForCapacity = function () {
+  var selectedIndex = roomNumberSelect.selectedIndex;
+  var capacityIndex = capacitySelect.selectedIndex;
+  if (selectedIndex < capacityIndex) {
+    switch (selectedIndex) {
+      case 0: capacitySelect.setCustomValidity('1 комната для 1 гостя');
+        break;
+      case 1: capacitySelect.setCustomValidity('2 комнаты «для 2 гостей» или «для 1 гостя»');
+        break;
+      case 2: capacitySelect.setCustomValidity('3 комнаты «для 3 гостей», «для 2 гостей» или «для 1 гостя»');
+        break;
+      default: break;
+    }
+  } else {
+    capacitySelect.setCustomValidity('');
+  }
+};
+
+roomNumberSelect.addEventListener('change', function () {
+  validateRoomsForCapacity();
+});
+
+capacitySelect.addEventListener('change', function () {
+  validateRoomsForCapacity();
+});
+
 addressInput.value = getPinAddress(mapPinMain, PIN_WIDTH, PIN_MAIN_DEFAULT_HEIGHT);
+setInactiveState();
