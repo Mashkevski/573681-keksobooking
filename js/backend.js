94da71c69b3070344534e20260c7f5d8de68a504
Siarhei Mashkevski <cybera@list.ru>
2018-07-01 23:25:50 +0300

    добавляет backend

---
+++ js/backend.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
+var UPLOAD_URL = 'https://js.dump.academy/keksobooking';
+
+window.backend = {
+  load: function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText + ', запрос не удался');
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('GET', LOAD_URL);
+    xhr.send();
+  },
+
+  upload: function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText + ', запрос не удался');
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('POST', UPLOAD_URL);
+    xhr.send(data);
+  }
+};
--- js/form.js
+++ js/form.js
@@ -4,6 +4,11 @@
   var addForm = document.querySelector('.ad-form');
   var addFormReset = document.querySelector('.ad-form__reset');
   var mapFilters = document.querySelector('.map__filters');
+  var type = document.querySelector('#type');
+  var timeInSelect = document.querySelector('#timein');
+  var timeOutSelect = document.querySelector('#timeout');
+  var roomNumberSelect = document.querySelector('#room_number');
+  var capacitySelect = document.querySelector('#capacity');
 
   addFormReset.addEventListener('click', function () {
     mapFilters.reset();
@@ -26,14 +31,9 @@
     }
   };
 
-  var type = document.querySelector('#type');
-
   type.addEventListener('change', function () {
     changeMinPrice(type.value);
   });
-
-  var timeInSelect = document.querySelector('#timein');
-  var timeOutSelect = document.querySelector('#timeout');
 
   timeInSelect.addEventListener('change', function () {
     var selectedIindex = timeInSelect.selectedIndex;
@@ -46,9 +46,6 @@
     var options = timeInSelect.options;
     options[selectedIindex].selected = true;
   });
-
-  var roomNumberSelect = document.querySelector('#room_number');
-  var capacitySelect = document.querySelector('#capacity');
 
   var validateRoomsForCapacity = function () {
     var selectedIndex = roomNumberSelect.selectedIndex;
