986b9693a749c0cb7f3f1a67676b6ccb807db752
Siarhei Mashkevski <cybera@list.ru>
2018-07-10 20:07:17 +0300

    исправляет файл в модуль IIFE

--- js/backend.js
+++ js/backend.js
@@ -1,56 +1,58 @@
 'use strict';
 
-var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
-var UPLOAD_URL = 'https://js.dump.academy/keksobooking';
+(function () {
+  var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
+  var UPLOAD_URL = 'https://js.dump.academy/keksobooking';
 
-window.backend = {
-  load: function (onLoad, onError) {
-    var xhr = new XMLHttpRequest();
+  window.backend = {
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
 
-    xhr.responseType = 'json';
+      xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText + ', запрос не удался');
-      }
-    });
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText + ', запрос не удался');
+        }
+      });
 
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
 
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
 
-    xhr.open('GET', LOAD_URL);
-    xhr.send();
-  },
+      xhr.open('GET', LOAD_URL);
+      xhr.send();
+    },
 
-  upload: function (data, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
+    upload: function (data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
 
-    xhr.responseType = 'json';
+      xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText + ', запрос не удался');
-      }
-    });
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText + ', запрос не удался');
+        }
+      });
 
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
 
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
 
-    xhr.open('POST', UPLOAD_URL);
-    xhr.send(data);
-  }
-};
+      xhr.open('POST', UPLOAD_URL);
+      xhr.send(data);
+    }
+  };
+})();
